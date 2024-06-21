60306f50d0c3d8341764d2d2975c746062aecda9
Davi <davisoaresdasilva1203@gmail.com>
2024-06-21 17:37:55 -0300

    Projeto 03

---
+++ Calculadora/README.md
@@ -0,0 +1,2 @@
+O projeto atual consiste em uma calculadora simples via terminal/console.
+    O usuário pode entrar com 2 valores inteiros e seguir com multiplicação, divisão, soma, subtração.
---
+++ Calculadora/src/CalculadoraSimples.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class CalculadoraSimples {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("* Calculadora *");
+
+        // Solicitando primeira entrada do user:
+        System.out.print("Digite o primeiro número: ");
+        int n1 = scan.nextInt();
+
+        // Solicitando segunda entrada do user:
+        System.out.print("Digite o segundo número: ");
+        int n2 = scan.nextInt();
+
+        // Apresentando operações:
+        System.out.println("Escolha uma operação ( + , - , * , / )");
+        String operacao = scan.next();
+
+        double resultado = 0;
+
+        switch (operacao) {
+            case "+":
+                resultado = n1 + n2;
+                break;
+            case "-":
+                resultado = n1 - n2;
+                break;
+            case "*":
+                resultado = n1 * n2;
+                break;
+            case "/":
+                resultado = n1 / n2;
+                break;
+
+            default:
+                System.out.println("Operação indisponivel. Tente novamente com um dos seguintes operadores: ");
+                System.out.println(" + , - , * , / ");
+                break;
+        }
+
+        System.out.println("O resultado da operação é: " + resultado);
+
+        scan.close();
+
+    }
+
+}
